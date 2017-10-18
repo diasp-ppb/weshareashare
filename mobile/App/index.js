96bb11b46a5821f32a9d0635adfda2bd1417e538
helder <helder_antunes-@hotmail.com>
2017-10-18 22:53:29 +0100

    added font roboto_medium.

--- mobile/App/index.js
+++ mobile/App/index.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { View } from 'react-native';
 import { Font } from 'expo';
+import Expo from 'expo';
 
 import PrimaryNav from './Navigation/AppNavigation.js';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+ 
   componentDidMount() {
-    Font.loadAsync({
+    Expo.Font.loadAsync({
       'OpenSans': require('./Assets/Fonts/OpenSans-Regular.ttf'),
       'OpenSans-Bold': require('./Assets/Fonts/OpenSans-Bold.ttf'),
       'OpenSans-Italic': require('./Assets/Fonts/OpenSans-Italic.ttf'),
     });
   }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'OpenSans': require('./Assets/Fonts/OpenSans-Regular.ttf'),
+      'OpenSans-Bold': require('./Assets/Fonts/OpenSans-Bold.ttf'),
+      'OpenSans-Italic': require('./Assets/Fonts/OpenSans-Italic.ttf'),
+      'Roboto_medium': require('./Assets/Fonts/Roboto_Medium.ttf'),
+    });
+
+    this.setState({ isReady: true });
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
     return (
       <PrimaryNav />
     );
