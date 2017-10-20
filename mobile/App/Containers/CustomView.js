5ea048c1773c2a8271c5e30134573e216d2409a3
inesfproenca <inesfproenca@gmail.com>
2017-10-20 14:14:00 +0100

    First warning corrected

--- mobile/App/Containers/CustomView.js
+++ mobile/App/Containers/CustomView.js
@@ -1,87 +1,94 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
-    Button,
-    StyleSheet,
-    TouchableOpacity,
-    ViewPropTypes,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  ViewPropTypes,
 } from 'react-native';
 
 
 export default class CustomView extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            options: [],
-            selected: null,
-            picked: false
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: [],
+      selected: null,
+      picked: false
+    };
+  }
+
+  componentWillMount() {
+    let options = this.props.currentMessage.selectOption;
+
+    if (options) {
+      if (this.state.selected === null && !this.state.picked) {
+        this.setState(options);
+      }
     }
+  };
 
-    render() {
-        let options = this.props.currentMessage.selectOption;
+  render() {
+    let options = this.props.currentMessage.selectOption;
 
-        if (options) {
-            if (this.state.selected === null && !this.state.picked) {
-                this.setState(options);
-            }
-            let Items = this.state.options.map((s, i) => {
+    if (options) {
+      let Items = this.state.options.map((s, i) => {
 
-                return <Button
-                    style={styles.button}
-                    onPress={() => {
+        return <Button
+        style={styles.button}
+        onPress={() => {
 
-                        this.setState({
-                            options: [s],
-                            selected: s,
-                            picked: true,
-                        });
+          this.setState({
+            options: [s],
+            selected: s,
+            picked: true,
+          });
 
-                        this.props.currentMessage.picked = true;
-                        this.props.onSend(s);
+          this.props.currentMessage.picked = true;
+          this.props.onSend(s);
 
-                    }
-                    }
-                    title={s}
-                    disabled={this.state.picked}
-                    color="#FFFFFF"
-                    accessibilityLabel={s}
-                />
-
-            });
-
-
-            return (
-                <TouchableOpacity style={[styles.container, this.props.containerStyle]}>
-                    {Items}
-                </TouchableOpacity>
-            );
         }
-        return null;
-    }
+      }
+      title={s}
+      disabled={this.state.picked}
+      color="#FFFFFF"
+      accessibilityLabel={s}
+      />
+
+    });
+
+
+    return (
+      <TouchableOpacity style={[styles.container, this.props.containerStyle]}>
+      {Items}
+      </TouchableOpacity>
+    );
+  }
+  return null;
+}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        borderRadius: 13,
-        marginTop: 8,
-        paddingTop: 5,
-    },
-    button: {
+  container: {
+    borderRadius: 13,
+    marginTop: 8,
+    paddingTop: 5,
+  },
+  button: {
 
-    },
+  },
 
 });
 
 CustomView.defaultProps = {
-    currentMessage: {},
-    containerStyle: {},
-    mapViewStyle: {},
+  currentMessage: {},
+  containerStyle: {},
+  mapViewStyle: {},
 };
 
 CustomView.propTypes = {
-    currentMessage: PropTypes.object,
-    containerStyle: ViewPropTypes.style,
-    mapViewStyle: ViewPropTypes.style,
+  currentMessage: PropTypes.object,
+  containerStyle: ViewPropTypes.style,
+  mapViewStyle: ViewPropTypes.style,
 };
