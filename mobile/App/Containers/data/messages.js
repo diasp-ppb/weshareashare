42bbc667dc73a95da265fd5174a4f3c6bd983676
rodavoce <up201404178@fe.up.pt>
2017-10-24 00:12:48 +0100

    multiple options on mid && options still hardcoded

--- mobile/App/Containers/data/messages.js
+++ mobile/App/Containers/data/messages.js
@@ -1,23 +1,13 @@
 module.exports = [
     {
         _id: Math.round(Math.random() * 1000000),
-        createdAt: new Date(Date.UTC(2016, 7, 30, 17, 21, 0)),
-        user: {
-            _id: 1,
-            name: 'FF',
-        },
-        selectOption: {
-            options: ['yes','no'],
-            selected: 'yes',
-            picked: false
-        }
-    },
-    {
-        _id: Math.round(Math.random() * 1000000),
         text: 'Do you want to start the subscription form? It will take around 7minutes.',
         user: {
             _id: 2,
             name: 'React Native',
+        },
+        selectOption: {
+            options: ['yes','no']
         }
     },
 ];
