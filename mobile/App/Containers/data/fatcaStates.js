69280e1a02ad63ce568c574b454b02f282579575
inesfproenca <inesfproenca@gmail.com>
2017-10-26 17:16:33 +0100

    Changed state structure

--- mobile/App/Containers/data/fatcaStates.js
+++ mobile/App/Containers/data/fatcaStates.js
@@ -1,19 +1,40 @@
 module.exports = {
-  questions:[
-    "What is your full name?",
-    "Tell me your NIF please",
+  states:[
+    {
+      question: 'What is your full name?',
+      answers: null
+    },
+    {
+      question: 'Tell me your NIF please',
+      answers: null
+    },
 
-    'Do you have U.S. American nacionality?',
-    'Do you possess a green card?',
-    'Do you have U.S. American passport?',
-    'Were you born in the USA or in a USA territory?',
-    'Are you a permanent resident in the USA? Or do you have a substancial presence in the USA (more than 183 days in the last 3 years, of which, 31 were in the current year)?',
+    //Is US Person questionaire
+    {
+      question: 'Do you have U.S. American nacionality?',
+      answers: ["Yes","No"],
+    },
+    {
+      question: 'Do you possess a green card?',
+      answers: ["Yes","No"],
+    },
+    {
+      question: 'Do you have U.S. American passport?',
+      answers: ["Yes","No"],
+    },
+    {
+      question: 'Were you born in the USA or in a USA territory?',
+      answers: ["Yes","No"],
+    },
+    {
+      question: 'Are you a permanent resident in the USA? Or do you have a substancial presence in the USA (more than 183 days in the last 3 years, of which, 31 were in the current year)?',
+      answers: ["Yes","No"],
+    },
 
-    'Are you a diplomat, foreign student, invited professor or professional athelete?',
-  ],
-  answers:[
-    "Yes",
-    "No"
+    {
+      question: 'Are you a diplomat, foreign student, invited professor or professional athelete?',
+      answers: ["Yes","No"],
+    }
   ],
 
 };
