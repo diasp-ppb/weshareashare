a801c1e3ee9c4e108d84007ed8ed033b874042dd
inesfproenca <inesfproenca@gmail.com>
2017-11-23 11:30:03 +0000

    Some much needed refatoring and still testing if pdf is okay

--- server/api/controllers/FundController.js
+++ server/api/controllers/FundController.js
@@ -27,7 +27,13 @@ module.exports = {
       let person = await Person.findOne({id: userid}).populate('subscription');
 
       let pdfTemplatePath = "../../resources/subscription_template.pdf";
-      let birthday = new Date(/*person.birthday*/);
+      let birthday;
+
+      if(person.birthday != null){
+        birthday = new Date(person.birthday);
+      }else{
+        birthday = new Date();
+      }
 
       let formData = {
         //Participante
@@ -72,6 +78,7 @@ module.exports = {
       fillPdf.generatePdf(formData, pdfTemplatePath, ['drop_xfa','need_appearances'], function(err, output) {
         if ( !err ) {
           console.log(formData);
+          //save output somewhere
         }
         else{
           throw err;
@@ -92,79 +99,5 @@ module.exports = {
   postInvestorProfile(req, res) {
     return res.ok();
   },
-
-  /*createSubscriptionPDF(req, res){
-    //let encoding = require('encoding');
-    let fillPdf = require("fill-pdf");
-
-    let parsedAttrs = Fund.parseAttrs(req.allParams());
-    let participantAttrs = parsedAttrs.participant;
-    let userid = req.headers['user-id'];
-
-    try {
-      let user = await User.findOne({id: userid});
-      let person = await Person.findOne({id: userid}).populate('subscription');
-
-      let pdfTemplatePath = "";
-      let birthday = new Date(person.birthday);
-
-      let formData = {
-        //Participante
-        Nome_Participante: person.name,
-        Genero_Participante: person.genre,
-        Morada_Participante: person.address,
-        Localidade_Participante: person.area,
-        Codigo_Postal_Participante: person.postal,
-        Telefone_Participante: person.telephone,
-        Telemovel_Participante: person.cellphone,
-        Contribuinte_Participante: person.NIF,
-        BI_Participante: person.identificationNumber,
-        Dia_Nascimento_Participante: birthday.getDate(),
-        Mes_Nascimento_Participante: birthday.getMonth(),
-        Ano_Nascimento_Participante: birthday.getFullYear(),
-        Profissao_Participante: person.profession,
-        Entidade_Patronal_Participante: person.employer,
-        Email_Participante: user.email,
-        Receber_Email: 'yes',
-        //Subscrição
-        Valor_Entrega: person.subscription.subscriptionValue,
-        //Comissao_Subscricao: 20,
-        //Comissao_Reembolso: 5,
-        Forma_Pagamento: person.subscription.paymentMethod, //'Transferencia' 'Deposito' ' Cheque'
-        Numero_Cheque: person.subscription.checkNo,
-        Banco: person.subscription.checkBank,
-        Transferencia_PPR: 'Yes',
-        //SDD
-        Nome: person.name,
-        IBAN: person.subscription.accountNo,
-        Valor_Mensal: person.subscription.debitAmount,
-        Crescimento_Anual: person.subscription.debitGrowth,
-        Periodicidade: person.subscription.periodicity, //'Mensal' 'Trimestral' 'Semestral' 'Anual'
-        Mes: 11,
-        Ano: 2017,
-      };
-
-      //formData.Nome_Participante = encoding.convert(formData.Nome_Participante, 'ISO-8859-1', 'UTF-8');
-      //formData.Morada_Participante = encoding.convert(formData.Morada_Participante, 'ISO-8859-1', 'UTF-8');
-      //formData.Nome = encoding.convert(formData.Nome, 'ISO-8859-1', 'UTF-8');
-
-      fillPdf.generatePdf(formData, pdfTemplatePath, ['drop_xfa','need_appearances'], function(err, output) {
-        if ( !err ) {
-          //res.type("application/pdf");
-          //res.send(output);
-        }
-        else{
-          throw err;
-        }
-      });
-
-      console.log(person);
-      console.log(formData);
-    } catch(err) {
-      return res.serverError(err);
-    }
-
-    return res.ok();
-  }*/
 
 };
