008a862d1106f1e38390f42514abbd8d48338003
Pedro Dias <up201404178@fe.up.pt>
2017-11-30 03:55:27 +0000

    added notification icon switch [ still needs redux ]

--- mobile/App/Components/HeaderRight.js
+++ mobile/App/Components/HeaderRight.js
@@ -1,24 +1,36 @@
 import React, {Component, PropTypes} from "react";
 import {
-  View,
-  TouchableOpacity,
+    View,
+    TouchableOpacity,
 } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from "./Styles/HeadBarStyle";
 
-export default class RightComponent extends Component {
-  render () {
-      const { navigate } = this.props.navigation;
 
-      return(
-      <View style={styles.sideByside}>
-        <TouchableOpacity style={styles.pullLeft}>
-          <Icon style= {styles.button} name="bell" size={30} color="#fff"/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigate("DrawerOpen")}>
-          <Icon style= {styles.button} name="menu" size={30} color="#fff"/>
-        </TouchableOpacity>
-      </View>
-    )
-  }
+
+export default class RightComponent extends Component {
+
+    NotificationIcon() {
+        var symbol = "bell-off";
+        if(this.props.currentState)
+            symbol="bell";
+        return symbol;
+    }
+
+    render () {
+        const { navigate } = this.props.navigation;
+
+        return(
+            <View style={styles.sideByside}>
+                <TouchableOpacity style={styles.pullLeft} onPress={()=> this.props.switchState}>
+                    <Icon style= {styles.button} name={this.NotificationIcon()} size={30} color="#fff"/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigate("DrawerOpen")}>
+                    <Icon style= {styles.button} name="menu" size={30} color="#fff"/>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+
 }
