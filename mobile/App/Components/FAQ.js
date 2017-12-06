bf0973dab81a580073b2d9664c61a93b8a274527
Pedro Pacheco <pedrodanielpacheco96@hotmail.com>
2017-12-06 15:24:15 +0000

    FAQ list interface.

--- mobile/App/Components/FAQ.js
+++ mobile/App/Components/FAQ.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
-import { ApplicationStyles } from '@theme/';
+import { View, ScrollView } from 'react-native';
+import { ApplicationStyles, Metrics, Assets } from '@theme/';
+import { Text } from '@ui/';
+import { Divider } from 'react-native-elements';
 
 export default class AboutUs extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -10,11 +11,38 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-      <View style={ApplicationStyles.mainContainer}>
         <View style={ApplicationStyles.container}>
+        <ScrollView>
 
+        <View style={[ApplicationStyles.row, ApplicationStyles.padding, ApplicationStyles.rightAligned]}>
+          <View style={ApplicationStyles.benefitItem} />
+          <Text h3>How to do this?</Text>
         </View>
-      </View>
+
+        <View style={[ApplicationStyles.row, ApplicationStyles.padding]}>
+        <Text >Qis simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+        </View>
+
+        <View style={[ApplicationStyles.row, ApplicationStyles.padding]}>
+          <View style={ApplicationStyles.benefitItem} />
+          <Text h3>What do I need to do?</Text>
+        </View>
+
+        <View style={[ApplicationStyles.row, ApplicationStyles.padding]}>
+        <Text >Qis simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+        </View>
+
+        <View style={[ApplicationStyles.row, ApplicationStyles.padding]}>
+          <View style={ApplicationStyles.benefitItem} />
+          <Text h3>What is the risk?</Text>
+        </View>
+
+        <View style={[ApplicationStyles.row, ApplicationStyles.padding]}>
+        <Text >Qis simply dummy text of the printing and typesetting industry. Lorem Ipsum has but as ska been the industry's standard dummy text ever since the 1500s</Text>
+        </View>
+
+        </ScrollView>
+        </View>
     );
   }
 }
