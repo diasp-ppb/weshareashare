9ff29ae81643de1c83faf692510191760f359382
Renato Abreu <renatoabreu1196@gmail.com>
2017-12-13 22:49:08 +0000

    simulation, i guess

--- mobile/App/Containers/Saving/Simulation/SimulationContainer.js
+++ mobile/App/Containers/Saving/Simulation/SimulationContainer.js
@@ -5,7 +5,67 @@
 import { connect } from 'react-redux';
 import FormRender from './SimulationView';
 
+let initialInvestment = [
+  {value: 0, label: "0"},
+  {value: 5000, label: "5000"},
+  {value: 10000, label: "10000"},
+  {value: 15000, label: "15000"},
+  {value: 20000, label: "20000"},
+  {value: 25000, label: "25000"},
+  {value: 30000, label: "30000"},
+  {value: 35000, label: "35000"},
+  {value: 40000, label: "40000"},
+  {value: 45000, label: "45000"},
+  {value: 50000, label: "50000"},
+  {value: 55000, label: "55000"},
+  {value: 60000, label: "60000"},
+];
+
+let monthlyContribution = [
+  {value: 100, label: "100"},
+  {value: 200, label: "200"},
+  {value: 300, label: "300"},
+  {value: 400, label: "400"},
+  {value: 500, label: "500"},
+  {value: 600, label: "600"},
+  {value: 700, label: "700"},
+  {value: 800, label: "800"},
+  {value: 900, label: "900"},
+  {value: 1000, label: "1000"},
+];
+
+let yearPeriod = [
+  {value: 5, label: "5"},
+  {value: 6, label: "6"},
+  {value: 7, label: "7"},
+  {value: 8, label: "8"},
+  {value: 9, label: "9"},
+  {value: 10, label: "10"},
+  {value: 11, label: "11"},
+  {value: 12, label: "12"},
+  {value: 13, label: "13"},
+  {value: 14, label: "14"},
+  {value: 15, label: "15"},
+];
+
+let interestRate = [
+  {value: 1, label: "1%"},
+  {value: 2, label: "2%"},
+  {value: 3, label: "3%"},
+  {value: 4, label: "4%"},
+  {value: 5, label: "5%"},
+  {value: 6, label: "6%"},
+  {value: 7, label: "7%"},
+  {value: 8, label: "8%"},
+  {value: 9, label: "9%"},
+  {value: 10, label: "10%"},
+]
+
 const mapStateToProps = (state) => ({
+  interestRate: interestRate,
+  initialInvestment: initialInvestment,
+  yearPeriod: yearPeriod,
+  monthlyContribution: monthlyContribution,
   session: state.session,
 });
 
--- mobile/App/Containers/Saving/Simulation/SimulationView.js
+++ mobile/App/Containers/Saving/Simulation/SimulationView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { ScrollView, TouchableOpacity } from 'react-native';
+import { ScrollView, Picker, View, TouchableOpacity } from 'react-native';
 import I18n from '@i18n/i18n';
-import { ApplicationStyles } from '@theme/';
+import { ApplicationStyles, Metrics, Colors } from '@theme/';
 import { Card, Text, Spacer } from '@ui/';
 import AppStep from '@components/AppStep';
 
@@ -10,11 +10,117 @@ export default class Simulation extends Component {
     title: I18n.t('simulation'),
   });
   
+  constructor (props){
+    super(props);
+    this.state = {
+      initialInvestment: this.props.initialInvestment[0].value,
+      monthlyContribution: this.props.monthlyContribution[0].value,
+      yearPeriod: this.props.yearPeriod[0].value,
+      interestRate: this.props.interestRate[0].value,
+      futureValue: 0
+    }
+  }
+  
+  componentDidMount () {
+    this.calculateFutureValue();
+  }
+  
+  addPickerItems = (items) => {
+    return items.map((item, key) => {
+      return (
+        <Picker.Item key={key} label={item.label} value={item.value} />
+      );
+    });
+  }
+  
+  convertToCurrency = () => {
+    let amount = parseFloat(this.state.futureValue).toFixed(2);
+    let splitAmount = amount.split(".")[0];
+    let i = splitAmount.length-4;
+    
+    while(i>=0){
+      splitAmount = splitAmount.slice(0,i+1) + "," + splitAmount.slice(i+1);
+      i=i-3;
+    }
+    return "\u20AC" + splitAmount + "." + amount.split(".")[1];
+  }
+  
+  calculateFutureValue = () => {
+    let r = this.state.interestRate / 100;
+    let n = this.state.yearPeriod;
+    let nt = n * 12;
+    let rn = r / parseFloat(12);
+    let newValue = this.state.monthlyContribution * ((Math.pow((1 + rn), nt) - 1) / rn);
+    this.setState({futureValue: newValue});
+  }
+  
   render () {
+    const inputHeight = Metrics.DEVICE_HEIGHT / 6;
     const { navigate } = this.props.navigation;
     return (
       <ScrollView style={ApplicationStyles.container}>
         <Card>
+          <View style={[{flexDirection: 'row'}]}>
+            <View style={[{flex: 1, flexDirection: 'column', marginRight: 5}]}>
+              <View style={[{alignItems: 'center', justifyContent: 'center', height: inputHeight}]}>
+                <Text h3 style={[ApplicationStyles.textCenterAligned]}>
+                  Se o seu investimento inicial for
+                </Text>
+              </View>
+              <Picker
+                selectedValue={this.state.initialInvestment}
+                onValueChange={(itemValue, itemIndex) => {
+                  this.setState({initialInvestment: itemValue}, () => this.calculateFutureValue())}}>
+                {this.addPickerItems(this.props.initialInvestment)}
+              </Picker>
+  
+              <View style={[{alignItems: 'center', justifyContent: 'center', height: inputHeight}]}>
+                <Text h3 style={[ApplicationStyles.textCenterAligned]}>
+                 durante
+                </Text>
+              </View>
+  
+              <Picker
+                selectedValue={this.state.yearPeriod}
+                onValueChange={(itemValue, itemIndex) => {
+                  this.setState({yearPeriod: itemValue}, () => this.calculateFutureValue())}}>
+                {this.addPickerItems(this.props.yearPeriod)}
+              </Picker>
+            </View>
+            <View style={[{flex: 1, flexDirection: 'column'}]}>
+              <View style={[{alignItems: 'center', justifyContent: 'center', height: inputHeight}]}>
+                <Text h3 style={[ApplicationStyles.textCenterAligned]}>
+                  e todos os meses contribuir
+                </Text>
+              </View>
+              <Picker
+                selectedValue={this.state.monthlyContribution}
+                onValueChange={(itemValue, itemIndex) => {
+                  this.setState({monthlyContribution: itemValue}, () => this.calculateFutureValue())}}>
+                {this.addPickerItems(this.props.monthlyContribution)}
+              </Picker>
+  
+              <View style={[{alignItems: 'center', justifyContent: 'center', height: inputHeight}]}>
+                <Text h3 style={[ApplicationStyles.textCenterAligned]}>
+                  com uma taxa anual de juros de
+                </Text>
+              </View>
+  
+              <Picker
+                selectedValue={this.state.interestRate}
+                onValueChange={(itemValue, itemIndex) => {
+                  this.setState({interestRate: itemValue}, () => this.calculateFutureValue())}}>
+                {this.addPickerItems(this.props.interestRate)}
+              </Picker>
+            </View>
+          </View>
+          
+          <View style={[{alignItems: 'center', justifyContent: 'center', paddingVertical: 20}]}>
+            <Text h2 style={[ApplicationStyles.textCenterAligned]}>
+              Em {this.state.yearPeriod} anos terá <Text h2 style={{color: Colors.stoikBlue}}>{this.convertToCurrency()}</Text>
+            </Text>
+          </View>
+         
           <TouchableOpacity style={ApplicationStyles.rightAligned} onPress={() => navigate('Benefits')}>
             <Text p style={ApplicationStyles.nextLink}>
               { I18n.t('benefits') } >
