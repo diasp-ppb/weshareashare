147e60086a1de99d6f7ecaff52776ab4239d2cc4
Pedro Dias <up201404178@fe.up.pt>
2018-01-06 18:07:12 +0000

    Some fix

--- mobile/App/Components/Homepage.js
+++ mobile/App/Components/Homepage.js
@@ -3,7 +3,7 @@ import { View, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import { Button, Text } from 'native-base';
 import I18n from '@i18n/i18n';
 import styles from './Styles/HomepageStyle';
-import { ApplicationStyles, Assets, Metrics } from '@theme/';
+import { ApplicationStyles, Assets, Metrics, Colors } from '@theme/';
 
 export default class Homepage extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -40,22 +40,22 @@ export default class Homepage extends Component {
 
               <View style={[{ flexDirection: 'row', paddingTop: Metrics.padding * 2 }]}>
                 <View style={[{ flex: 1, alignItems: 'center', justifyContent: 'center' }]}>
-                  <TouchableOpacity style={{ flex: 1 }} onPress={() => navigate('ContactUs')}>
+                  <TouchableOpacity style={{ flex: 1, backgroundColor: Colors.stoikGrey }} onPress={() => navigate('ContactUs')}>
                     <Text p style={[ApplicationStyles.link]}>
                       { I18n.t('contactUs') }
                     </Text>
                   </TouchableOpacity>
                 </View>
                 <View style={[{ flex: 1, alignItems: 'center', justifyContent: 'center' }]}>
-                  <TouchableOpacity style={{ flex: 2 }} onPress={() => navigate('AboutUs')}>
+                  <TouchableOpacity style={{ flex: 2 , backgroundColor: Colors.stoikGrey}} onPress={() => navigate('AboutUs')}>
                     <Text p style={[ApplicationStyles.link]}>
                       { I18n.t('aboutUs') }
                     </Text>
                   </TouchableOpacity>
                 </View>
                 <View style={[{ flex: 1, alignItems: 'center', justifyContent: 'center' }]}>
-                  <TouchableOpacity style={{ flex: 3 }} onPress={() => navigate('RiskWarnings')}>
-                    <Text p style={[ApplicationStyles.link]}>
+                  <TouchableOpacity style={{ flex: 3, backgroundColor: Colors.stoikGrey }} onPress={() => navigate('RiskWarnings')}>
+                    <Text p style={[ApplicationStyles.link, {textAlign: 'center'}]}>
                       { I18n.t('riskWarnings') }
                     </Text>
                   </TouchableOpacity>
